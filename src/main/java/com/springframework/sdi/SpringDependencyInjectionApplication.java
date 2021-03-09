85711f69f3d15cce72d9b6db82e319bf41c66068
Haris Rio <harisbaskar1998@gmail.com>
2021-03-09 19:19:06 +0530

    Added mycontroller

--- src/main/java/com/springframework/sdi/SpringDependencyInjectionApplication.java
+++ src/main/java/com/springframework/sdi/SpringDependencyInjectionApplication.java
@@ -2,12 +2,18 @@ package com.springframework.sdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.springframework.sdi.controller.MyController;
 
 @SpringBootApplication
 public class SpringDependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
