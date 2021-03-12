5914156ae6dda95caf7cc08703d9d34afaae1e76
Haris Rio <harisbaskar1998@gmail.com>
2021-03-12 17:07:20 +0530

    Default profiles

--- src/main/java/com/springframework/sdi/service/I18nEnglishServiceImpl.java
+++ src/main/java/com/springframework/sdi/service/I18nEnglishServiceImpl.java
@@ -3,7 +3,7 @@ package com.springframework.sdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class I18nEnglishServiceImpl implements GreetingService{
 
